14fcd0b58adbb0e6e06646f85e23b7d8e41ad51e
Marco <marco.wplm@gmail.com>
2016-11-01 21:44:37 +0100

    created first recommender from teacher's code

--- Matlab/test.m
+++ Matlab/test.m
@@ -1,7 +1,9 @@
 clear; close all;
 
-importInteractions;
-importUsers;
+[filename, pathname] = uigetfile('*.csv', 'Enter path of interactions.csv: ');
+eval(['cd ',pathname]);
+interactions = importInteractions(filename);
+targetusers = importUsers('D:\Users\Marco\Documents\GitHub\recsys-2016\csv\target_users.csv');
 toDelete = interactions.interaction_type<3;
 interactions(toDelete, :) = [];
 [G, ID] = findgroups(interactions);
